679f16aaa60cc017f4fdd54cb779709c2a4ed25f
forhad39 <forhadmd6482@gmail.com>
2026-05-10 11:23:22 +0600

    Fix BASE_URL by adding trailing slash

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 import time
 from datetime import datetime, timezone
 
-BASE_URL = "https://kingstarclub.sbs"
+BASE_URL = "https://kingstarclub.sbs/"
 
 def run(script):
     url = BASE_URL + script
